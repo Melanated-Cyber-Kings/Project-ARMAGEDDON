4251b4e1d16c679925d0d22ff6513f636759c232
BashiM1 <mbas2097152@outlook.com>
2026-01-10 17:12:50 +0000

    Removed SG rules from Security module

--- LAB1/a/modules/security/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_sg_id" {
-  description = "ID of EC2 Security Group"
-  value       = aws_security_group.ec2.id
-}
-
-output "rds_sg_id" {
-  description = "ID of RDS Security Group"
-  value       = aws_security_group.rds.id
-}
