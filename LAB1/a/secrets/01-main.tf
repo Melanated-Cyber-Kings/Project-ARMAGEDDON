620b8551d51254176bff8fa2b8c56cd0a2ceab3d
Brimah Khalil Kamara <brimahk@gmail.com>
2026-01-26 20:53:46 +0000

    RECENT

--- LAB1/a/secrets/01-main.tf
+++ LAB1/a/secrets/01-main.tf
@@ -9,3 +9,15 @@ resource "aws_secretsmanager_secret" "rds_secret" {
 data "aws_secretsmanager_secret" "my_secret" {
   name = "${var.env_prefix}/rds/mysql"
 }
+
+resource "aws_secretsmanager_secret_version" "rds_secret_version" {
+  secret_id = aws_secretsmanager_secret.rds_secret.id
+
+  secret_string = jsonencode({
+    username = var.username
+    password = var.password
+    #host     = var.address
+    port     = var.port
+    dbname   = var.dbname
+  })
+}
--- LAB1/a/secrets/03-variables.tf
+++ LAB1/a/secrets/03-variables.tf
@@ -6,6 +6,39 @@ variable "region" {
 variable "env_prefix" {
   description = "project environment"
   type = string
-  #default = "lab-1a"
+  default = "lab-1a"
+
+  validation {
+    condition = contains(["lab-1a", "lab-1b", "lab-1c"], var.env_prefix)
+      error_message = "The environment must be one of: lab-1a, lab-1b or lab-1c"
+  }
 }
 
+
+variable "username" {
+  description = "RDS master username"
+  type        = string
+  sensitive   = true
+}
+
+variable "password" {
+  description = "RDS master password"
+  type        = string
+  sensitive   = true
+}
+
+#variable "address" {
+#description = "RDS endpoint or hostname"
+#  type        = string
+#}
+
+variable "port" {
+  description = "Database port"
+  type        = number
+  default     = 3306
+}
+
+variable "dbname" {
+  description = "Database name"
+  type        = string
+}
